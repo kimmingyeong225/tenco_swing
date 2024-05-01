a644d9f06619d4ae61a185f256246da42696b345
maze <you@kred730036@gmail.com>
2024-05-01 15:03:37 +0900

    예제

---
+++ src/ch08/GameTest.java
@@ -0,0 +1,13 @@
+package ch08;
+
+public class GameTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("----");
+		new GameFrame();
+		
+
+	}// end of main
+
+}// end of class
