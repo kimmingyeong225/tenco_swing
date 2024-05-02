8394a2589a7f8a11f4c302fcbc5ec97071a3a56e
kmk <kred730036@gmail.com>
2024-05-02 14:15:26 +0900

    bubble 2단계 완성

---
+++ src/Bubble/ex02/Moveable.java
@@ -0,0 +1,10 @@
+package Bubble.ex02;
+
+public interface Moveable {
+
+	public abstract void left();
+	public abstract void rigth();
+	public abstract void up();
+	public abstract void down();
+	
+}
